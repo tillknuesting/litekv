2205d95d5290717ca8f873682abf65dc870a8e80
knuesting.till.ext@siemens.com <tillknuesting@gmail.com>
2023-03-22 21:42:06 +0000

    make data and index public

--- kv.go
+++ kv.go
@@ -18,7 +18,7 @@ const (
 	// RecordTypeNormal represents a normal record, which contains a key-value pair.
 	RecordTypeNormal RecordType = iota
 
-	// RecordTypeDeleted represents a deleted record, which is marked as deleted but not removed from the data slice.
+	// RecordTypeDeleted represents a deleted record, which is marked as deleted but not removed from the Data slice.
 	RecordTypeDeleted
 )
 
@@ -50,22 +50,22 @@ const (
 	ErrorKeyNotFound = Error("key not found")
 
 	// ErrorChecksumMismatch is returned when a record's calculated checksum
-	// does not match its stored checksum, indicating data corruption.
+	// does not match its stored checksum, indicating Data corruption.
 	ErrorChecksumMismatch = Error("checksum mismatch")
 )
 
 // KeyValueStore is a simple key-value store implementation.
-// It utilizes a byte slice (data) to store serialized records and a map (index) to map keys to their position in the data byte slice.
+// It utilizes a byte slice (Data) to store serialized records and a map (Index) to map keys to their position in the Data byte slice.
 // The KeyValueStore struct also embeds the sync.RWMutex to ensure thread safety during concurrent read and write operations.
 type KeyValueStore struct {
 	sync.RWMutex                  // Embed the RWMutex to ensure thread safety during concurrent read and write operations.
-	data         []byte           // A byte slice that holds the serialized records.
-	index        map[string]int64 // A map that maps keys (as strings) to their position in the data byte slice.
+	Data         []byte           // A byte slice that holds the serialized records.
+	Index        map[string]int64 // A map that maps keys (as strings) to their position in the Data byte slice.
 }
 
 // Write takes a key and a value, both in byte slices, and stores them in the KeyValueStore instance.
 // This method is responsible for creating a new Record with the given key-value pair and appending it
-// to the data byte slice. It also updates the index map to map the key to the position of the new record.
+// to the Data byte slice. It also updates the Index map to map the key to the position of the new record.
 func (kvs *KeyValueStore) Write(key, value []byte) {
 	// First, create a new record with the given key and value.
 	// Set the record type to "RecordTypeNormal" to indicate that it's a normal record, not a deleted one.
@@ -85,16 +85,16 @@ func (kvs *KeyValueStore) Write(key, value []byte) {
 	// Defer the unlocking operation so that the lock is released after the method finishes.
 	defer kvs.Unlock()
 
-	// Check if the index map is initialized, and if not, initialize it.
-	if kvs.index == nil {
-		kvs.index = make(map[string]int64)
+	// Check if the Index map is initialized, and if not, initialize it.
+	if kvs.Index == nil {
+		kvs.Index = make(map[string]int64)
 	}
 
-	// Update the index map to associate the key with the current position in the data byte slice.
-	kvs.index[string(key)] = int64(len(kvs.data))
+	// Update the Index map to associate the key with the current position in the Data byte slice.
+	kvs.Index[string(key)] = int64(len(kvs.Data))
 
-	// Convert the record to a byte slice and append it to the data byte slice.
-	kvs.data = append(kvs.data, record.toBytes()...)
+	// Convert the record to a byte slice and append it to the Data byte slice.
+	kvs.Data = append(kvs.Data, record.toBytes()...)
 }
 
 // Read takes a key in the form of a byte slice and retrieves the associated value from the KeyValueStore instance.
@@ -105,17 +105,17 @@ func (kvs *KeyValueStore) Read(key []byte) ([]byte, error) {
 	// Defer the unlocking operation so that the lock is released after the method finishes.
 	defer kvs.RUnlock()
 
-	// Use the index map to find the position of the record associated with the given key.
-	// If the key doesn't exist in the index map, return an ErrorKeyNotFound error.
-	pos, exists := kvs.index[string(key)]
+	// Use the Index map to find the position of the record associated with the given key.
+	// If the key doesn't exist in the Index map, return an ErrorKeyNotFound error.
+	pos, exists := kvs.Index[string(key)]
 	if !exists {
 		return nil, ErrorKeyNotFound
 	}
 
-	// Create a new bytes.Buffer with the data from the position found in the index map.
-	buf := bytes.NewBuffer(kvs.data[pos:])
+	// Create a new bytes.Buffer with the Data from the position found in the Index map.
+	buf := bytes.NewBuffer(kvs.Data[pos:])
 
-	// Initialize a new Record instance and deserialize the record data from the bytes.Buffer.
+	// Initialize a new Record instance and deserialize the record Data from the bytes.Buffer.
 	record := new(Record)
 	record.fromBytes(buf)
 
@@ -133,8 +133,8 @@ func (kvs *KeyValueStore) Read(key []byte) ([]byte, error) {
 }
 
 // Delete takes a key in the form of a byte slice and marks the associated record as deleted in the KeyValueStore instance.
-// It achieves this by creating a new Record with the RecordType set to RecordTypeDeleted and appending it to the data byte slice.
-// It also updates the index map to map the key to the position of the new deleted record.
+// It achieves this by creating a new Record with the RecordType set to RecordTypeDeleted and appending it to the Data byte slice.
+// It also updates the Index map to map the key to the position of the new deleted record.
 func (kvs *KeyValueStore) Delete(key []byte) {
 	// Create a new Record with the given key and set the record type to "RecordTypeDeleted" to indicate that it's a deleted record.
 	record := &Record{
@@ -151,11 +151,11 @@ func (kvs *KeyValueStore) Delete(key []byte) {
 	// Defer the unlocking operation so that the lock is released after the method finishes.
 	defer kvs.Unlock()
 
-	// Update the index map to associate the key with the current position in the data byte slice.
-	kvs.index[string(key)] = int64(len(kvs.data))
+	// Update the Index map to associate the key with the current position in the Data byte slice.
+	kvs.Index[string(key)] = int64(len(kvs.Data))
 
-	// Convert the record to a byte slice and append it to the data byte slice.
-	kvs.data = append(kvs.data, record.toBytes()...)
+	// Convert the record to a byte slice and append it to the Data byte slice.
+	kvs.Data = append(kvs.Data, record.toBytes()...)
 }
 
 // calculateChecksum calculates the CRC checksum of a Record instance.
@@ -200,7 +200,7 @@ func (r *Record) toBytes() []byte {
 }
 
 // fromBytes deserializes a byte slice into a Record instance.
-// It takes a bytes.Buffer containing the serialized record data as an input,
+// It takes a bytes.Buffer containing the serialized record Data as an input,
 // reads the record fields in little-endian byte order, and stores them in the corresponding fields of the Record instance.
 func (r *Record) fromBytes(buf *bytes.Buffer) {
 	// Read the record fields from the buffer in little-endian byte order.
@@ -211,28 +211,28 @@ func (r *Record) fromBytes(buf *bytes.Buffer) {
 	binary.Read(buf, binary.LittleEndian, &r.ValueLength)
 
 	// Allocate memory for the Key and Value fields based on their lengths,
-	// then read the Key and Value data from the buffer into the allocated memory.
+	// then read the Key and Value Data from the buffer into the allocated memory.
 	r.Key = make([]byte, r.KeyLength)
 	r.Value = make([]byte, r.ValueLength)
 	buf.Read(r.Key)
 	buf.Read(r.Value)
 }
 
-// SaveIndex serializes the KeyValueStore's index (a map of keys to their position in the data byte slice)
-// using the gob package, and returns the serialized index as a byte slice.
-// This method can be used to persist the index to disk or another storage medium, for later restoration.
+// SaveIndex serializes the KeyValueStore's Index (a map of keys to their position in the Data byte slice)
+// using the gob package, and returns the serialized Index as a byte slice.
+// This method can be used to persist the Index to disk or another storage medium, for later restoration.
 func (kvs *KeyValueStore) SaveIndex() ([]byte, error) {
 	kvs.Lock()
 	defer kvs.Unlock()
 
-	// Create a new bytes.Buffer to store the serialized index.
+	// Create a new bytes.Buffer to store the serialized Index.
 	var buf bytes.Buffer
 
 	// Create a new gob.Encoder that writes to the buffer.
 	encoder := gob.NewEncoder(&buf)
 
-	// Encode the KeyValueStore's index using the gob.Encoder.
-	err := encoder.Encode(kvs.index)
+	// Encode the KeyValueStore's Index using the gob.Encoder.
+	err := encoder.Encode(kvs.Index)
 	// If there's an error during encoding, return nil and the error.
 	if err != nil {
 		return nil, err
@@ -242,27 +242,27 @@ func (kvs *KeyValueStore) SaveIndex() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// LoadIndex deserializes a byte slice containing a serialized index (a map of keys to their position in the data byte slice)
-// using the gob package, and restores the deserialized index to the KeyValueStore.
-// This method can be used to load a previously saved index from disk or another storage medium.
+// LoadIndex deserializes a byte slice containing a serialized Index (a map of keys to their position in the Data byte slice)
+// using the gob package, and restores the deserialized Index to the KeyValueStore.
+// This method can be used to load a previously saved Index from disk or another storage medium.
 func (kvs *KeyValueStore) LoadIndex(data []byte) error {
 	kvs.Lock()
 	defer kvs.Unlock()
 
-	// Create a new bytes.Buffer initialized with the input data.
+	// Create a new bytes.Buffer initialized with the input Data.
 	buf := bytes.NewBuffer(data)
 
 	// Create a new gob.Decoder that reads from the buffer.
 	decoder := gob.NewDecoder(buf)
 
-	// Decode the serialized index using the gob.Decoder into the KeyValueStore's index.
-	err := decoder.Decode(&kvs.index)
+	// Decode the serialized Index using the gob.Decoder into the KeyValueStore's Index.
+	err := decoder.Decode(&kvs.Index)
 	// If there's an error during decoding, return the error.
 	if err != nil {
 		return err
 	}
 
-	// Return no error, indicating that the index was successfully loaded.
+	// Return no error, indicating that the Index was successfully loaded.
 	return nil
 }
 
@@ -273,10 +273,10 @@ func (kvs *KeyValueStore) findLatestRecords() map[string]*Record {
 	// Initialize a set to store deleted keys.
 	deletedKeys := make(map[string]struct{})
 
-	// Create a new bytes.Buffer initialized with the KeyValueStore's data.
-	buf := bytes.NewBuffer(kvs.data)
+	// Create a new bytes.Buffer initialized with the KeyValueStore's Data.
+	buf := bytes.NewBuffer(kvs.Data)
 
-	// Iterate through the data byte slice as long as there are remaining bytes.
+	// Iterate through the Data byte slice as long as there are remaining bytes.
 	for buf.Len() > 0 {
 		// Create a new Record.
 		record := new(Record)
@@ -307,8 +307,8 @@ func (kvs *KeyValueStore) findLatestRecords() map[string]*Record {
 	return latestRecords
 }
 
-// Compact iterates through the KeyValueStore's data byte slice, identifies the latest records for each key,
-// and rebuilds the data slice and index, effectively removing any deleted or outdated records.
+// Compact iterates through the KeyValueStore's Data byte slice, identifies the latest records for each key,
+// and rebuilds the Data slice and Index, effectively removing any deleted or outdated records.
 // This method is useful for reducing the storage size and improving the performance of the KeyValueStore.
 func (kvs *KeyValueStore) Compact() {
 	// Acquire a lock on the KeyValueStore to ensure thread safety.
@@ -318,44 +318,44 @@ func (kvs *KeyValueStore) Compact() {
 	// Find the latest records for each key using the findLatestRecords method.
 	latestRecords := kvs.findLatestRecords()
 
-	// Rebuild the data slice and index by initializing them as empty.
-	kvs.data = make([]byte, 0)
-	kvs.index = make(map[string]int64)
+	// Rebuild the Data slice and Index by initializing them as empty.
+	kvs.Data = make([]byte, 0)
+	kvs.Index = make(map[string]int64)
 
 	// Iterate through the latest records.
 	for key, record := range latestRecords {
 		// Check if the record is of type RecordTypeNormal.
 		if record.Type == RecordTypeNormal {
-			// Calculate the new position for the key in the data slice.
-			pos := int64(len(kvs.data))
+			// Calculate the new position for the key in the Data slice.
+			pos := int64(len(kvs.Data))
 
-			// Update the index with the new position.
-			kvs.index[key] = pos
+			// Update the Index with the new position.
+			kvs.Index[key] = pos
 
-			// Append the record's serialized form to the data slice.
-			kvs.data = append(kvs.data, record.toBytes()...)
+			// Append the record's serialized form to the Data slice.
+			kvs.Data = append(kvs.Data, record.toBytes()...)
 		}
 	}
 }
 
-// RebuildIndex iterates through the KeyValueStore's data byte slice, deserializes each record,
-// and rebuilds the index by calculating the position of each key in the data slice.
-// This method is useful when the index has been lost or corrupted and needs to be reconstructed.
+// RebuildIndex iterates through the KeyValueStore's Data byte slice, deserializes each record,
+// and rebuilds the Index by calculating the position of each key in the Data slice.
+// This method is useful when the Index has been lost or corrupted and needs to be reconstructed.
 func (kvs *KeyValueStore) RebuildIndex() {
 	// Acquire a lock on the KeyValueStore to ensure thread safety.
 	kvs.Lock()
 	defer kvs.Unlock()
 
-	// Create a new bytes.Buffer initialized with the KeyValueStore's data.
-	buf := bytes.NewBuffer(kvs.data)
+	// Create a new bytes.Buffer initialized with the KeyValueStore's Data.
+	buf := bytes.NewBuffer(kvs.Data)
 
-	// Initialize a new empty index.
-	kvs.index = make(map[string]int64)
+	// Initialize a new empty Index.
+	kvs.Index = make(map[string]int64)
 
-	// Initialize a variable to track the position of the current record in the data byte slice.
+	// Initialize a variable to track the position of the current record in the Data byte slice.
 	var pos int64
 
-	// Iterate through the data byte slice as long as there are remaining bytes.
+	// Iterate through the Data byte slice as long as there are remaining bytes.
 	for buf.Len() > 0 {
 		// Create a new Record.
 		record := new(Record)
@@ -366,8 +366,8 @@ func (kvs *KeyValueStore) RebuildIndex() {
 		// Convert the byte slice key to a string.
 		key := string(record.Key)
 
-		// Update the index with the key's position in the data byte slice.
-		kvs.index[key] = pos
+		// Update the Index with the key's position in the Data byte slice.
+		kvs.Index[key] = pos
 
 		// Calculate the size of the current record (Crc, Type, KeyLength, ValueLength, Key, and Value fields).
 		recordSize := int64(4 + 1 + 4 + 4 + len(record.Key) + len(record.Value))
@@ -377,7 +377,7 @@ func (kvs *KeyValueStore) RebuildIndex() {
 	}
 }
 
-// PrintAllKeyValuePairs iterates through the KeyValueStore's data byte slice, deserializes each record,
+// PrintAllKeyValuePairs iterates through the KeyValueStore's Data byte slice, deserializes each record,
 // and prints the key, value, and record type for each record.
 // This method is useful for debugging and getting an overview of the KeyValueStore's contents.
 func (kvs *KeyValueStore) PrintAllKeyValuePairs() {
@@ -385,10 +385,10 @@ func (kvs *KeyValueStore) PrintAllKeyValuePairs() {
 	kvs.RLock()
 	defer kvs.RUnlock()
 
-	// Create a new bytes.Buffer initialized with the KeyValueStore's data.
-	buf := bytes.NewBuffer(kvs.data)
+	// Create a new bytes.Buffer initialized with the KeyValueStore's Data.
+	buf := bytes.NewBuffer(kvs.Data)
 
-	// Iterate through the data byte slice as long as there are remaining bytes.
+	// Iterate through the Data byte slice as long as there are remaining bytes.
 	for buf.Len() > 0 {
 		// Create a new Record.
 		record := new(Record)
